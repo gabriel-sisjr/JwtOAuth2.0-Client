09dcf1a65b035632877dd5a003707ffd61e8642f
Gabriel Santana <Gabriel.sistemasjr@gmail.com>
2020-08-27 10:35:09 -0300

    Commitando estrutura do código!

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace JwtOAuth2._0
 {
@@ -25,6 +22,23 @@ namespace JwtOAuth2._0
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Setando configuração OAuth2
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                o.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+            })
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://jpproject-sso:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiSecret = "api-secret";
+                    options.ApiName = "report-api";
+                    options.RoleClaimType = JwtClaimTypes.Role;
+                    options.NameClaimType = JwtClaimTypes.Name;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +51,9 @@ namespace JwtOAuth2._0
 
             app.UseRouting();
 
+            // Setando autorização e autenticação
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
